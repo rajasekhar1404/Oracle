673893a501dc33997453c26d83f843e39fb87f4b
rajasekhar1404 <mrrajasekhar09@gmail.com>
2022-07-06 16:34:37 +0530

    plsql_basics updated

--- PLSQL/Basic.sql
+++ PLSQL/Basic.sql
@@ -4,3 +4,16 @@ BEGIN
     marks := :val;
     DBMS_OUTPUT.PUT_LINE('Total marks obtained: ' || marks);
 END;
+
+--
+/*
+Print all the employees who are living in HYD.
+*/
+DECLARE
+    emp_info Employee%ROWTYPE;
+    v_dept VARCHAR2(30);
+BEGIN
+    SELECT dept_name INTO v_dept FROM departments WHERE location = 'BLR';
+    SELECT * INTO emp_info FROM employee WHERE dept_name = v_dept AND ROWNUM = 1;
+    DBMS_OUTPUT.PUT_LINE(emp_info.emp_name || chr(10) || emp_info.emp_id || chr(10) || emp_info.dept_name || chr(10) || emp_info.salary || ' are working in HYD.');
+END;
--- SQL/Basic.sql
+++ SQL/Basic.sql
@@ -1,18 +1,37 @@
-SELECT ROWNUM, a.* FROM (
-SELECT * FROM (
-SELECT ROWNUM rno, Employee.* FROM Employee
-) ORDER BY rno DESC) a;
+SELECT * FROM departments;
+SELECT * FROM Employee;
 
-SELECT ROWNUM rno, Employee.* FROM Employee
+SELECT e.emp_id, e.emp_name, e.dept_name, d.location, e.salary
+FROM employee e, departments d
+WHERE e.dept_name = d.dept_name AND e.dept_name = 'Admin';
 
-SELECT emp_id, emp_name, dept_name, salary FROM (
-SELECT ROWNUM rno, a.* FROM (
-SELECT emp_id, emp_name, dept_name, DISTINCT salary FROM employee ORDER BY salary DESC) a ) WHERE rno = 5;
+SELECT * FROM employee WHERE dept_name IN 
+(SELECT dept_name FROM departments
+WHERE location = 'HYD' OR location = 'BLR');
 
-SELECT * FROM Employee WHERE salary = (
-SELECT salary FROM (
-SELECT ROWNUM rno, a.* FROM (
-SELECT DISTINCT salary FROM employee ORDER BY salary DESC) a)
-WHERE rno = 4);
 
-SELECT * FROM EMPLOYEE;
+SELECT * FROM DIVISION;
+SELECT * FROM MERCH;
+SELECT * FROM BUYER;
+
+INSERT INTO buyer VALUES(1700, 'Steve', 6335352352, 3523523543);
+
+SELECT b.*, m.*, d.* 
+FROM buyer b, merch m, division d
+WHERE b.buyer = d.buyer AND
+d.merch = m.merch;
+
+SELECT * FROM BUYER 
+WHERE buyer_fax IS NULL;
+
+SELECT COUNT(buyer), buyer
+FROM buyer 
+GROUP BY buyer
+HAVING COUNT(buyer) > 1;
+
+
+
+DELETE FROM buyer WHERE rowid not in(
+SELECT MAX(rowid)
+FROM buyer 
+GROUP BY buyer);
